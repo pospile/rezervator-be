a6ef27889727c5432211834714867e0414f5bf59
Vojtěch Pospíchal <vojtech.pospichal@zonky.cz>
2019-04-02 14:21:41 +0200

    Removed rancher support added swarm support

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 for ((n=1;n<2;n++))
 do
-    echo "node-${n}"
-    docker-machine create --driver "digitalocean" --digitalocean-image "ubuntu-16-04-x64" --digitalocean-region "FRA1" --digitalocean-access-token $DO_TOKEN "node-1"
+    echo "node-${n}";
+    docker-machine create --driver "digitalocean" --digitalocean-image "ubuntu-16-04-x64" --digitalocean-region "FRA1" --digitalocean-access-token $DO_TOKEN "node-${n}";
 done;
+
+docker-machine create --driver "digitalocean" --digitalocean-image  "ubuntu-16-04-x64" --digitalocean-region "fra1" --digitalocean-access-token $DOTOKEN manager-1
